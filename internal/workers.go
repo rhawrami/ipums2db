31967d424751fff342d35ad61052460d81314e93
r h <ravanhawrami@gmail.com>
2025-11-04 20:02:01 -0600

    added doc comm

--- internal/workers.go
+++ internal/workers.go
@@ -5,6 +5,11 @@ import (
 	"os"
 )
 
+// WriteToDumpFile reads from a channel of parsed blocks, and then writes those
+// blocks to an output file. The channel it reads from contains ParsedResults,
+// which can contain errors. In case of errors, it prints out the error and continues.
+//
+// Returns error if a block write  cannot be completed.
 func WriteToDumpFile(outFile *os.File, parsedStream chan ParsedResult) error {
 	for res := range parsedStream {
 		if res.AnyError != nil {
@@ -24,6 +29,8 @@ func WriteToDumpFile(outFile *os.File, parsedStream chan ParsedResult) error {
 	return nil
 }
 
+// ParseBlock wraps the DatabaseFormatter.BulkInsert method, now reading from a channel of
+// ParsingJobs, and writing to a channel of ParsedResults. Each ParseBlock call gets a file header.
 func ParseBlock(dbfmtr DatabaseFormatter, ddi *DataDict, cfg ParserConfig) {
 	datFile, err := os.Open(cfg.DatFileName)
 	if err != nil {
@@ -37,11 +44,15 @@ func ParseBlock(dbfmtr DatabaseFormatter, ddi *DataDict, cfg ParserConfig) {
 	}
 }
 
+// A ParsedResult contains a block of fixed-width data parsed to SQL inserts,
+// and an error if applicable.
 type ParsedResult struct {
 	Block    []byte
 	AnyError error
 }
 
+// A ParserConfig contains the name of a fixed-width file, a channel of jobs that a ParseBlock call
+// should read from, and a channel of parsed results that a ParseBlock call should write to.
 type ParserConfig struct {
 	DatFileName  string
 	JobStream    chan ParsingJob

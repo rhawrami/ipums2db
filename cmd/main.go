766cc68d2b0ab42bb6b2af4fe16fc527c83e40e4
r h <ravanhawrami@gmail.com>
2025-11-04 21:44:24 -0600

    decent working program now; still need ot add nWriters

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 	"sync"
 	"time"
 
-	i2db "github.com/rhawrami/ipums2db/internal"
+	棕熊 "github.com/rhawrami/ipums2db/internal"
 )
 
 func main() {
@@ -41,87 +41,68 @@ func main() {
 	datFileName := cmdArgs[0]
 
 	start := time.Now()
-	func() {
-		if silentProg {
-			return
-		}
-		delim := "-----------------------------"
-		fmt.Printf(
-			"%s\ndbT: %s\ntab: %s\nidx: %s\nxml: %s\ndat: %s\n%s\n",
-			delim, dbType, tabName, indices, ddiPath, datFileName, delim,
-		)
-	}()
-	// print load
-	go func(silent bool) {
-		if silent {
-			return
-		}
-		printStatement := []byte("I-P-U-M-S-!")
-		downTime := time.Millisecond * 400
-		clearSpaces := strings.Repeat(" ", len(printStatement))
-		for {
-			for i := range printStatement {
-				fmt.Printf("\r%s", string(printStatement[:(i+1)]))
-				time.Sleep(downTime)
-			}
-			fmt.Printf("\r%s", clearSpaces)
-		}
-	}(silentProg)
-	// File creations; table and index creations
-	//
-	// dat file
-	datFile, err := os.Open(datFileName)
-	checkErr(err, "ipums2db: datFile")
-	// open the ddi; make DataDict
-	ddi, err := i2db.NewDataDict(ddiPath)
-	checkErr(err, "ipums2db: ddiFile")
-	// make DBFMTR
-	dbfmtr, err := i2db.NewDBFormatter(dbType, tabName)
-	checkErr(err, "ipums2db: formatter")
+
+	// print job summary
+	棕熊.PrintJobSummary(silentProg, "=", dbType, tabName, indices, ddiPath, datFileName)
+	// print loading message
+	棕熊.PrintLoadingMessage(silentProg)
+
+	// new DataDict and DatabaseFormatter
+	ddi, dbfmtr, err := 棕熊.NewDataDictAndDatabaseFormatter(dbType, tabName, ddiPath)
+	checkErr(err, "ipums2db: DataDict/DBFormatter")
+
+	// get dat file and total bytes in file
+	totBytes, err := 棕熊.TotalBytes(datFileName)
+	checkErr(err, "ipums2db: .dat file")
+	// get total bytes
+	bytesPerRow := 棕熊.BytesPerRow(&ddi)
+
 	// make outFile
-	dumpFile, err := createDumpFile(outFile)
+	dumpFile, err := 棕熊.CreateDumpFile(outFile)
 	checkErr(err, "ipums2db: dumpFile")
-	// defer dat and out close
-	defer dumpFile.Close()
-	defer datFile.Close()
+
 	// write main table creation, reference tables creation/insert, indices
-	err = writeSchemaAndIndices(dumpFile, dbfmtr, &ddi, idx)
+	err = 棕熊.WriteDDL(dumpFile, dbfmtr, &ddi, idx)
 	checkErr(err, "ipums2db: DDL")
 
-	// get total bytes
-	stats, err := datFile.Stat()
-	checkErr(err, "ipums2db: totBytes error")
-	totBytes := stats.Size()
-	bytesPerRow := i2db.BytesPerRow(&ddi)
+	// goroutines
+	// nWriters
+	nWriters := 1 // come back to this
+	// job config
+	jobConfig := 棕熊.NewJobConfig(totBytes, nWriters)
+	// parsing config
+	maxBPerJob := jobConfig.MaxBytesPerJob
+	parsedBlocksChanSize := jobConfig.ParsedResChanSize
+	nParsers := jobConfig.NumParsers
+
 	// chans
-	jobStream := make(chan i2db.ParsingJob)
-	parsedBlockStream := make(chan i2db.ParsedResult, 6)
+	jobStream := make(chan 棕熊.ParsingJob)
+	parsedBlockStream := make(chan 棕熊.ParsedResult, parsedBlocksChanSize)
 	// waitgroups
-	var makeJobWG sync.WaitGroup
-	var parseWG sync.WaitGroup
-	var writeWG sync.WaitGroup
+	var makeJobWG, parseWG, writeWG sync.WaitGroup
+
 	// spawn JobMaker
 	makeJobWG.Add(1)
 	go func() {
 		defer makeJobWG.Done()
-		err := i2db.MakeParsingJobsStream(bytesPerRow, int(totBytes), 1_000_000, jobStream)
+		err := 棕熊.MakeParsingJobsStream(bytesPerRow, int(totBytes), maxBPerJob, jobStream)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "ipums2db: parsing: %v\n", err)
 			os.Exit(1)
 		}
 	}()
+
 	// spawn Parsers
-	pConfig := i2db.ParserConfig{
+	pConfig := 棕熊.ParserConfig{
 		DatFileName:  datFileName,
 		JobStream:    jobStream,
 		ParsedStream: parsedBlockStream,
 	}
-	numParsers := 5
-	parseWG.Add(numParsers)
-	for i := 0; i < numParsers; i++ {
+	parseWG.Add(nParsers)
+	for i := 0; i < nParsers; i++ {
 		go func() {
 			defer parseWG.Done()
-			i2db.ParseBlock(*dbfmtr, &ddi, pConfig)
+			棕熊.ParseBlock(*dbfmtr, &ddi, pConfig)
 		}()
 	}
 
@@ -132,75 +113,33 @@ func main() {
 	}()
 
 	// spawn Writer
-	writeWG.Add(1)
-	go func() {
-		defer writeWG.Done()
-		err := i2db.WriteToDumpFile(dumpFile, parsedBlockStream)
-		checkErr(err, "ipums2db: writer")
-	}()
+	writeWG.Add(nWriters)
+	for i := 0; i < nWriters; i++ {
+		go func() {
+			defer writeWG.Done()
+			err := 棕熊.WriteToDumpFile(dumpFile, parsedBlockStream)
+			checkErr(err, "ipums2db: writer")
+		}()
+	}
 
+	// wait on each of the three steps
 	parseWG.Wait()
 	makeJobWG.Wait()
 	writeWG.Wait()
 
 	end := time.Now()
-	printFinalSummary(silentProg, start, end, int(totBytes), outFile)
+	棕熊.PrintFinalSummary(silentProg, start, end, int(totBytes), outFile)
 }
 
-func printFinalSummary(silent bool, start, end time.Time, totBytes int, dumpFile string) {
-	if silent {
-		return
-	}
-	timeElapsed := end.Sub(start).Round(time.Millisecond)
-	bytesInMiB := (2 << 19)
-	MiBPerSec := float64(totBytes) / timeElapsed.Seconds() / float64(bytesInMiB)
-	fmt.Printf("\rTime elapsed: %v (%.2f MiB/s)\nDump written to: %s\n", timeElapsed, MiBPerSec, dumpFile)
-}
-
+// Helper Functions
+// checkErr checks if err != nil; prints error and exits if so
 func checkErr(err error, topic string) {
 	if err != nil {
 		log.Fatalf("%v: %v\n", topic, err)
 	}
 }
 
-func createDumpFile(dumpFileName string) (*os.File, error) {
-	file, err := os.Create(dumpFileName)
-	if err != nil {
-		return nil, err
-	}
-	return file, nil
-}
-
-func writeSchemaAndIndices(dumpFile *os.File, dbfmtr *i2db.DatabaseFormatter, ddi *i2db.DataDict, indices []string) error {
-	// main table creation
-	tableSQL, err := dbfmtr.CreateMainTable(ddi)
-	if err != nil {
-		return fmt.Errorf("ipums2db: table creation: %w", err)
-	}
-	// ref tables
-	refTablesSQL, err := dbfmtr.CreateRefTables(ddi)
-	if err != nil {
-		return fmt.Errorf("ipums2db: reference tables creation: %w", err)
-	}
-	// indices
-	indicesSQL, err := dbfmtr.CreateIndices(ddi, indices)
-	if err != nil {
-		return fmt.Errorf("ipums2db: index creation: %w", err)
-	}
-
-	buffer := make([]byte, 0, len(tableSQL)+len(refTablesSQL)+len(indicesSQL))
-	buffer = append(buffer, tableSQL...)
-	buffer = append(buffer, refTablesSQL...)
-	buffer = append(buffer, indicesSQL...)
-
-	_, err = dumpFile.Write(buffer)
-	if err != nil {
-		return fmt.Errorf("ipums2db: DDL write: %v", err)
-	}
-
-	return nil
-}
-
+// checkDDIFlag checks if the ddi path is empty
 func checkDDIFlag(ddiF string) {
 	if len(ddiF) == 0 {
 		fmt.Printf("ipums2db: -ddi: must pass path to XML file (e.x. -ddi cps_001.xml)\n")
@@ -208,6 +147,7 @@ func checkDDIFlag(ddiF string) {
 	}
 }
 
+// parseIndicesFlag returns the comma-delimited indices flag argument as a string slice
 func parseIndicesFlag(indF string) []string {
 	if len(indF) == 0 {
 		return []string{}
@@ -216,6 +156,7 @@ func parseIndicesFlag(indF string) []string {
 	return indices
 }
 
+// checkOneArg checks if either there is more than one argument provided, or if no arguments are provided
 func checkOneArg(args []string) {
 	if len(args) > 1 {
 		fmt.Printf("ipums2db: args: only provide one argument (path to .dat file)\n")
